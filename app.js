e72cc0ea44c038f5395cf1d931ab607f91a58e55
SoniEx2 <endermoneymod@gmail.com>
2022-09-10 10:05:54 -0300

    Fix the thing not working

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 // Copyright (C) 2022 Soni L. - 0BSD
-document.onDOMContentLoaded = function() {
+document.addEventListener("DOMContentLoaded", function() {
 	const client = new WebTorrent();
-	console.log("Hello!");
 	const handler = function(e) {
-		console.log(e);
+		const loc = window.location;
+		const counter = document.createElement("p");
+		counter.textContent = e.target.innerText + " downloading...";
+		const target = document.getElementById('status');
+		target.append(counter);
+		client.add(loc.origin + "/" + e.target.value, {
+			strategy: "rarest"
+		}, function(t) {
+			t.on('download', function(bytes) {
+				counter.textContent = 
+					e.target.innerText +
+					" just downloaded: " + bytes +
+					" total downloaded: " + t.downloaded +
+					" download speed: " + t.downloadSpeed +
+					" progress: " + t.progress;
+			});
+			t.on('done', function() {
+				counter.replaceChildren(e.target.innerText);
+				t.files.forEach(function(f) {
+					if (f.name.endsWith(".iso")) {
+						f.getBlobURL(function(err, url) {
+							if (err) throw err;
+							const a = document.createElement("a");
+							a.textContent = f.name;
+							a.download = f.name;
+							a.href = url;
+							counter.append(" ", a);
+						});
+					}
+				});
+			});
+		});
 	};
 	Array.prototype.forEach.call(
 		document.getElementsByClassName("torrent"),
 		function(el) { el.onclick = handler },
 	);
-};
+});
